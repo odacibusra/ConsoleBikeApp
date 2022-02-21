a556da67305b9e7900c874c9aa4d690eeebadfc3
Busra Odaci <sbusrasubasi@gmail.com>
2022-02-21 12:13:25 +0300

    edit

--- ConsoleBikeApp/Program.cs
+++ ConsoleBikeApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleBikeApp
 {
     class Program
     {
-        public async static Task CallBikeIndexApi(int cityId)
+        public async static Task CallBikeIndexApi(int cityId, int distance)
         {
             var cityName = GetCityName(cityId);
 
@@ -20,8 +20,8 @@ namespace ConsoleBikeApp
             }
             else
             {
-                string uri = "https://bikeindex.org:443/api/v3/search/count?location={0}&stolenness=stolen";
-                string formattedUri = string.Format(uri, cityName);
+                string uri = "https://bikeindex.org:443/api/v3/search/count?location={0}&distance={1}&stolenness=stolen";
+                string formattedUri = string.Format(uri, cityName, distance);
 
                 using (var client = new HttpClient())
                 {
@@ -63,14 +63,16 @@ namespace ConsoleBikeApp
         {
             Console.WriteLine("**The cities we currently operate** \n 1-Amsterdam, The Netherlands \n 2-Berlin, Germany \n 3-Copenhagen, Denmark \n 4-Brussels, Belgium \n");
             Console.WriteLine("**The cities we want to expand in the next year** \n 5-Milan, Italy \n 6-London, UK \n 7-Paris, France \n \n");
-            Console.WriteLine("Enter the city number which you want to see the stolen bike count");
 
             while (true)
             {
                 try
                 {
+                    Console.WriteLine("Enter the city number which you want to see the stolen bike count");
                     var cityId = Convert.ToInt32(Console.ReadLine());
-                    CallBikeIndexApi(cityId).Wait();
+                    Console.WriteLine("Enter a distance to calculate get stolen bike count");
+                    var distance = Convert.ToInt32(Console.ReadLine());
+                    CallBikeIndexApi(cityId, distance).Wait();
                     Console.WriteLine(@"Press enter ""C"" to continue or any key to exit.");
                     var input = Console.ReadLine().ToUpper();
                     if (input != "C")
